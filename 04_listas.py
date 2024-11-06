730cb34e581c8d4d946f08e8be9df4e1ad17d448
renatocuellar <matealva@gmail.com>
2024-11-06 15:30:39 -0500

    Comenzamos a trabajar con listas

---
+++ 04_listas.py
@@ -0,0 +1,15 @@
+# Listas
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.85, "Renato", "Cuellar"]
+
+print(my_other_list)
