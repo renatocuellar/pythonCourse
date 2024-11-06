54b943c16df74fa88e867ffd6ea206462b4c0327
renatocuellar <matealva@gmail.com>
2024-11-06 17:53:18 -0500

    Comenzamos a trabajar con tuplas

---
+++ 05_tuplas.py
@@ -0,0 +1,24 @@
+# Tuples
+
+my_tuple = tuple()
+my_other_tuple = ()
+
+my_tuple = (35, 1.85, "Renato", "Cuéllar")
+my_other_tuple = (16, 25, 85, 32, 35)
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])
+print(my_tuple[-1])
+
+print(my_tuple.count("Matealva"))
+print(my_tuple.count("Cuéllar"))
+print(my_tuple.index("Renato"))
+
+# my_tuple[1] = 1.84 Las tuplas son inmutables
+# print(my_tuple) Las listas si se pueden cambiar
+
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple)
+
+print(my_sum_tuple[3:6])
