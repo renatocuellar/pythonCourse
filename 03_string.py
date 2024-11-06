258d78c783d527f42c507fafe82012e331d055c2
renatocuellar <matealva@gmail.com>
2024-11-06 11:59:34 -0500

    Todo el tipo de operaciones con strings

--- 03_string.py
+++ 03_string.py
@@ -14,3 +14,42 @@ name, surname, age = "Renato", "Cuéllar", 35
 
 print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d"%(name, surname, age))
+
+#Inferencia de datos (anteponiendo la f)
+
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+
+# Desempaquetado de caracteres
+
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(b)
+
+#división
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[1:2:4]
+print(language_slice)
+
+#Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
+
+# Funciones
+
+print(language.capitalize())
+print(language.upper())
+print(language.count(language))
+print(language.isnumeric)
+print(language.lower())
