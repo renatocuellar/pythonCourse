b19f27fd3f1263fecc947522ecc22f17f69e5b47
renatocuellar <matealva@gmail.com>
2024-11-05 19:13:22 -0500

    primeros tipos de datos

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 # El siguiente es un hola mundo
 print ("Hola Python")
+
+print(type("Soy un dato str")) # tipo de dato "str"
+print(type(5)) #tipo de dato "int"
+print(type(1.5)) #tipo de dato "float"
+print(type(True)) #tipo de dato "bool"
