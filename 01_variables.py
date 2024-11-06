276c5a40fe497fa02959ebd2bcff326079c08308
renatocuellar <matealva@gmail.com>
2024-11-05 21:34:04 -0500

    cadena de texto

---
+++ 01_variables.py
@@ -0,0 +1,22 @@
+# Este es el modulo de variables
+
+MyVariable = "My String variable"
+print(MyVariable)
+
+My_int_variable = 5
+print(My_int_variable)
+
+My_int_to_str_variable = str(My_int_variable)
+print(My_int_to_str_variable)
+print(type(My_int_to_str_variable))
+
+#concateación de variables en un print
+print(MyVariable, My_int_variable)
+
+#Función de operación llamada len, hace un conteo
+print(len(MyVariable))
+
+#variables en una sola linea
+name, surname, alias, edad = "Renato", "Cuellar", "matealva", 35
+print(name, surname, "Mi alias es: ", alias, "y mi edad es: ", edad)
+
