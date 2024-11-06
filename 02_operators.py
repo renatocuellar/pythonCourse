e472758c33d2da48ca2bc061972bda31d14d74c3
renatocuellar <matealva@gmail.com>
2024-11-05 22:28:12 -0500

    Tenemos ahora operadores más complejos

--- 02_operators.py
+++ 02_operators.py
@@ -1,4 +1,4 @@
-# Operadores
+# Operadores aritmeticos
 
 print(5 + 8)
 print(5 - 8)
@@ -7,6 +7,21 @@ print(5 / 8)
 print(5 % 8)
 print(8 // 2)
 print(5 ** 8)
+print(8 ** 3 + 3 - 10 / 2 // 3)
 
 print("Hola" + " python")
 print("Hola " + str(166))
+print("Hola " * 4)
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+#operadores comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
