f3d6dd0dd735bcbe73bf859fcc21251a6ba98073
renatocuellar <matealva@gmail.com>
2024-11-06 09:12:32 -0500

    Tercer ejercicio, calcular el cuadrado y el cubo de un numero

---
+++ 02.3_ejercicios.py
@@ -0,0 +1,10 @@
+"""
+Número Elevado al Cuadrado y al Cubo
+Pide al usuario que ingrese un número. Calcula el cuadrado y el cubo de ese número y muestra ambos resultados. Usa el operador ** para elevar el número.
+"""
+
+numero = int(input("¿Cuál es el número que quieres elevar al cuadrado y al cubo? "5))
+cuadrado = numero ** 2
+cubo = numero ** 3
+
+print("El número elevado al cuadrado es ", cuadrado, " Y el número elevado al cubo es ", cubo)
