3945d7b065b0f83bfdd8a575c5b18a7b8bc216d6
renatocuellar <matealva@gmail.com>
2024-11-06 09:02:44 -0500

    Primer ejercicio de pasar temperatura de un celsius a Fahrenheit

---
+++ 02.1_ejercicios.py
@@ -0,0 +1,12 @@
+"""
+Conversión de Temperatura
+Escribe un programa que pida una temperatura en grados Celsius y la convierta a Fahrenheit usando la fórmula:
+Fahrenheit=(Celsius×9/5)+32
+Muestra el resultado en pantalla.
+
+"""
+
+Celsius = float(input("¿Cuá42l es la tempratura en grados Celsius?"))
+Fahrenheir = (Celsius*9/5)+32
+
+print(Fahrenheir)
