5bf466020fe968980a20e9f264e055be7c79840e
renatocuellar <matealva@gmail.com>
2024-11-06 17:35:53 -0500

    Difernetes tipos de listas

--- 04_listas.py
+++ 04_listas.py
@@ -13,3 +13,21 @@ print(len(my_list))
 my_other_list = [35, 1.85, "Renato", "Cuellar"]
 
 print(my_other_list)
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-3])
+print(my_list.count(30))
+
+age, height, name, surname = my_other_list
+print(name)
+
+my_other_list.append("Matealva")
+print(my_other_list)
+
+my_other_list.insert(1, "Azul")
+print(my_other_list)
+
+my_other_list.remove("Azul")
+print(my_other_list)
