d9bad08045ffa78a24344a3d4d665229d8bd4806
renatocuellar <matealva@gmail.com>
2024-11-07 17:58:11 -0500

    Aprendimos condicionales

--- 08_Condicionals.py
+++ 08_Condicionals.py
@@ -6,12 +6,14 @@ my_condition = False
 if my_condition: #Es lo mismo que if my_condition == True
     print("Se ejecuta la condición del if")
 
-my_condition = 5 * 2
+my_condition = 5 * 5
 
-if my_condition > 10:
-    print("Se ejecuta la condición del segundo if")
+if my_condition > 10 and my_condition < 20:
+    print("Es maypor a 10 y menos que 20")
+elif my_condition == 25:
+    print("Es igual a 25")
 else:
-    print("Es menor o igual a 10")
+    print("Es menor o igual a 10 o mayor o igual a 20")
 
 
 print("la ejecución continúa")
