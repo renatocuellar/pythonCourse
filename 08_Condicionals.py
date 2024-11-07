835da6b86e0003eb9734c1da64926acaeca38e25
renatocuellar <matealva@gmail.com>
2024-11-07 15:27:44 -0500

    Comenzamos a trabajar con condicionales

--- 08_Condicionals.py
+++ 08_Condicionals.py
@@ -1,2 +1,15 @@
 # Condicionales
+# Si se cumple una condicion, se ejecuta
 
+my_condition = False
+
+if my_condition: #Es lo mismo que if my_condition == True
+    print("Se ejecuta la condición del if")
+
+my_condition = 5 * 2
+
+if my_condition == 10:
+    print("Se ejecuta la condición del segundo if")
+
+
+print("la ejecución continúa")
