05da667454066180809f8d5063bd17b7b510c281
renatocuellar <matealva@gmail.com>
2024-11-08 13:25:10 -0500

    Aprendimos a trabajar con while y for

--- 09_loops.py
+++ 09_loops.py
@@ -17,3 +17,33 @@ while my_condition < 20:
         print("Mi condición es 16")
         break
     print(my_condition)
+
+# Bucle FOR
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+for element in my_list: #Listas
+    print(element)
+
+my_tuple = (35, 1.85, "Renato", "Cuéllar")
+
+for element in my_tuple: #Tuplas
+    print(element)
+
+my_other_set = {"Renato", "Cuéllar", 35}
+
+for element in my_other_set: #Sets
+    print(element)
+
+
+my_other_dict = {"Nombre": "Renato", "Apellido": "Cuéllar", "Edad": 35, 1: "Python"}
+
+for element in my_other_dict: #Diccionarios
+    print(element)
+
+for element in my_other_dict.values():
+    print(element)
+    if element == 36:
+        break
+else:
+    print("El bucle for ha finalizado")
