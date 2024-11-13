76ebc6945780daa49297489ecd9b49984ec72683
renatocuellar <matealva@gmail.com>
2024-11-13 18:58:54 -0500

    Hemos aprendido a importar ficheros

---
+++ 13_modules.py
@@ -0,0 +1,9 @@
+# Modules
+# Podemos importar ficheros desde fuera, en esta caso, 
+# lo que hacemos es crear una nueva hoja de llamada module.py
+
+import module
+from module import sum_two_values
+
+module.sum(5, 7, 18)
+module.sum_two_values(5, 2)
---
+++ module.py
@@ -0,0 +1,6 @@
+
+def sum (numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+def sum_two_values (first_numer, second_number):
+    print(first_numer ** second_number)
